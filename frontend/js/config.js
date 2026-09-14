8b92f4a1c64345eb732cf841c04002bd7b27567a
OrelAI <ubriga@gmail.com>
2026-09-14 05:09:27 +0300

    Point API_BASE at production backend

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -2,7 +2,7 @@
 // After deploying the backend to PythonAnywhere, set API_BASE to its URL,
 // e.g. "https://<username>.pythonanywhere.com"
 const CONFIG = {
-  API_BASE: "http://127.0.0.1:5000",
+  API_BASE: "https://ubriga.pythonanywhere.com",
   // Google OAuth Web Client ID (same value as the backend GOOGLE_CLIENT_ID).
   GOOGLE_CLIENT_ID: "609382927099-k7b75i2igf0ka0t0ohknfa6svlcp5s29.apps.googleusercontent.com",
   POLL_MS: 1500,
