507154d422182d1fcfe84c5295113bcdb58258eb
OrelAI <ubriga@gmail.com>
2026-09-14 04:57:27 +0300

    Set Google OAuth client ID

--- js/config.js
+++ js/config.js
@@ -4,6 +4,6 @@
 const CONFIG = {
   API_BASE: "http://127.0.0.1:5000",
   // Google OAuth Web Client ID (same value as the backend GOOGLE_CLIENT_ID).
-  GOOGLE_CLIENT_ID: "",
+  GOOGLE_CLIENT_ID: "609382927099-k7b75i2igf0ka0t0ohknfa6svlcp5s29.apps.googleusercontent.com",
   POLL_MS: 1500,
 };
